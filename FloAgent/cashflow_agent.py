fbd967fec3d3fa895682d26068b88c4efd65f261
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-04-08 23:58:01 +0500

    Define tool for agent to fetch transactions from db

--- FloAgent/cashflow_agent.py
+++ FloAgent/cashflow_agent.py
@@ -3,9 +3,10 @@ from dataclasses import dataclass
 from dotenv import load_dotenv, find_dotenv
 import asyncio
 import os
-from context import UserData, UserFinanceContext
-from db.mongo import db
+from FloAgent.context import UserFinanceContext
+from db.mongo import database
 from bson import ObjectId
+
 # RunHooks, AgentHooks,
 
 load_dotenv(find_dotenv())
@@ -32,13 +33,22 @@ config = RunConfig(
 @function_tool
 def get_transactions(ctx: RunContextWrapper[UserFinanceContext]):
     """Fetch all transactions for the current user."""
+
+    print("🔧 [TOOL CALL] get_transactions was executed")
+    print(f"   User ID: {ctx.context.userId}")
+
+    print(f"DB name: {database.name}")
+    print(f"Collections: {database.list_collection_names()}")
+
     userId = ctx.context.userId
-    transactions = list(db.transactions.find(
+    transactions = list(database.transactions.find(
         {"userId": ObjectId(userId)},
         {"_id": 0, "userId": 0}  # exclude these fields
     ))
     if not transactions:
         return "No transactions found for this user."
+    
+    print(f"   → Returned {len(transactions)} transactions")  # Nice to see
     return transactions
 
 # @function_tool
@@ -67,27 +77,39 @@ def get_transactions(ctx: RunContextWrapper[UserFinanceContext]):
 #     except Exception as e:
 #         print(f"There was an error calling the tool:{e}")
 
-async def kickoff(question: str):
+async def kickoff(question: str, userID: str, name: str, email: str):
+  
+#   name: str = 'Dawood Ayaz'
+#   email : str = 'dawoodayaz18@gmail.com'
 
   try:
-    Medical_Assistant: Agent = Agent(
-    name="AI Assistant",
-    instructions=f"You are a helpful AI assistant",
+    user_context = UserFinanceContext(
+            userId=userID,
+            name=name,
+            email=email
+        )
+    
+    CashFlow_Assistant: Agent = Agent[UserFinanceContext](
+    name="Cash Flow Assistant",
+    instructions=f"You are CashFlow Assistant, a smart and friendly personal finance assistant. You have access to their complete financial data through the tools provided",
     model=model,
+    tools=[get_transactions]
     )
 
     result = await Runner.run(
-      Medical_Assistant, 
+      CashFlow_Assistant, 
       question,
-      run_config=config
+      run_config=config,
+      context= user_context 
     )
     print(result.final_output)
     return result.final_output
   except Exception as e:
       print(f"There was an error connecting the Server:{e}")
 
-# ". After checking all the data of the user, provide personalized medical advice and answer health-related questions based on the provided {user_data}, {user_diet}, and {illnesses}. Ensure your advice is tailored to the user's age, health condition, diet, and known allergies. Always prioritize the user's safety and well-being. Do not provide a diagnosis or prescribe medication. If a question is outside your scope, advise the user to consult a medical professional."
-
-if __name__ == '__main__':
-  question1 : str = "How can I create an AI model through openai-agents sdk, briefly answer in simple steps."
-  asyncio.run(kickoff(question1))
+# if __name__ == '__main__':
+#   question1 : str = "Are you able to access my database with my userID and name because I have some transactions made recently that I saved in it, if yes, then tell me about them"
+#   userID : str = '69ba9e8e3427be64889d8d2b'
+#   name: str = 'Dawood Ayaz'
+#   email : str = 'dawoodayaz18@gmail.com' 
+#   asyncio.run(kickoff(question1, userID, name, email))
--- agents/__init__.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from cashflowassistant!")
-
-
-if __name__ == "__main__":
-    main()
