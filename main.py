e2adf6fd647c7c70abab6006d77b38b58cb06a39
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-04-15 02:20:29 +0500

    add user_name, email in question_request class.

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from FloAgent.cashflow_agent import kickoff
 from pydantic import BaseModel
+from dotenv import load_dotenv, find_dotenv
+import os
 
+load_dotenv(find_dotenv())
 app = FastAPI()
+request_origin = os.getenv("request_origin")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[f"{request_origin}"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -15,14 +19,15 @@ app.add_middleware(
 
 class QuestionRequest(BaseModel):
     question: str
-    userId: str
-    name: str
+    userID: str
+    user_name: str
     email: str
 
 @app.post("/floAssistant")
 async def ask_question(request: QuestionRequest):
     try:
-        result = await kickoff(request.question, request.userId, request.name, request.email)
+        
+        result = await kickoff(request.question, request.userID, request.user_name, request.email)
         return result
     except Exception as e:
         print(f"Error running your request: {e}")
